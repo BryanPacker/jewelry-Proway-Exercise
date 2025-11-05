6792145b1a8a05128fce49b23cbdd987b3bcb9dd
asdasd <erfef@gmail.com>
2025-11-04 21:22:14 -0300

    Last changes project cdn,s3

--- modulo7-iac_tooling/main.tf
+++ modulo7-iac_tooling/main.tf
@@ -11,93 +11,168 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "jewelry-vpc" {
-  cidr_block       = "10.0.1.0/16"
-  region = "us-east-1"
+# Bucket S3
+resource "aws_s3_bucket" "jewelry-bucket" {
+  bucket        = "jewelry-store-bucket-bryan"
+  force_destroy = true
+}
 
-  tags = {
-    Name = "jewelry-vpc-bryan"
+# Bloquear acesso público
+resource "aws_s3_bucket_public_access_block" "jewelry-bucket" {
+  bucket = aws_s3_bucket.jewelry-bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Upload index.html
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.jewelry-bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+# CloudFront OAC
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "jewelry-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# WAF Web ACL
+resource "aws_wafv2_web_acl" "cloudfront_waf" {
+  name  = "jewelry-cloudfront-waf"
+  scope = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  # Rate Limiting: máximo 2000 requisições por IP em 5 minutos
+  rule {
+    name     = "rate-limit"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 2000
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "RateLimitRule"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  # Proteção contra SQL Injection, XSS, etc
+  rule {
+    name     = "aws-managed-rules"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        vendor_name = "AWS"
+        name        = "AWSManagedRulesCommonRuleSet"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSetMetric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "JewelryWAF"
+    sampled_requests_enabled   = true
   }
 }
 
-resource "aws_subnet" "jewelry-subnet" {
-  vpc_id     = aws_vpc.jewelry-vpc.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
-  tags = {
-    Name = "jewelry-subnet-bryan"
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+  web_acl_id          = aws_wafv2_web_acl.cloudfront_waf.arn
+
+  origin {
+    domain_name              = aws_s3_bucket.jewelry-bucket.bucket_regional_domain_name
+    origin_id                = "S3"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3"
+    viewer_protocol_policy = "allow-all"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
 
-resource "aws_security_group" "jewelry-sec-group" {
-  name        = "jewelry-sg-bryan"
-  vpc_id      = aws_vpc.jewelry-vpc.id
-  tags = {
-    Name = "jewelry-sec-group-bryan"
-  }
+# Policy S3
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.jewelry-bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid    = "AllowCloudFront"
+      Effect = "Allow"
+      Principal = {
+        Service = "cloudfront.amazonaws.com"
+      }
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.jewelry-bucket.arn}/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.cdn.arn
+        }
+      }
+    }]
+  })
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.jewelry-sec-group.id
-  cidr_ipv4         = aws_vpc.jewelry-vpc.cidr_block
-  from_port         = 8080
-  ip_protocol       = "tcp"
-  to_port           = 8080
+# Outputs
+output "url" {
+  value = "http://${aws_cloudfront_distribution.cdn.domain_name}"
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "jewelry-bryan" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  associate_public_ip_address = "true"
-  security_groups = [ aws_security_group.jewelry-sec-group.id]
-  subnet_id = aws_subnet.jewelry-subnet.id
-  user-data = <<-EOF 
-
-    #!/bin/bash
-    apt update
-    apt install -y docker.io git build-essential
-    systemctl start docker
-    systemctl enable docker
-    docker container stop jewelry-app 2> /dev/null
-    cd /home
-    rm -rf proway-docker/
-    git clone https://github.com/BryanPacker/jewelry-Proway-Exercise.git
-    cd ./modulo7-iac_tooling
-    make docker-run
-
-  EOF
-
-  tags = {
-    Name = "jewelry-instance-bryan"
-  }
-} 
-
-output "vm_public_ip" {
-  value = aws_instance.jewelry-bryan.public_ip
-}
-
-output "app_url" {
-  value = "http://${aws_instance.jewelry-bryan.public_ip}:8080"
+output "waf_id" {
+  value = aws_wafv2_web_acl.cloudfront_waf.id
 }
